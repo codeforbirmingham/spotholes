424198cb1cfee76a94a0aba610a48791227a4779
Justin Harrison <justin.harrison1337@gmail.com>
2015-01-13 00:29:56 -0600

    adding attr_accessible to admin_user model to prevent errors in database migration when creating the default AdminUser

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,12 +2,14 @@ class AdminUser < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
-devise :database_authenticatable,
+  attr_accessible :email, :password, :password_confirmation
+
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
-	after_create { |admin| admin.send_reset_password_instructions }
- 
-	def password_required?
-	  new_record? ? false : super
-	end
+  after_create { |admin| admin.send_reset_password_instructions }
+
+  def password_required?
+    new_record? ? false : super
+  end
 end

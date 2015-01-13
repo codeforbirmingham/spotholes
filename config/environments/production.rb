7add7b795d87359dafe9d2cea1e2788bac6abd64
Justin Harrison <justin.harrison1337@gmail.com>
2015-01-13 01:46:10 -0600

    updating the application configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {host: ENV['SES_HOSTNAME']}
+  config.action_mailer.default_url_options = {host: ENV['APP_HOSTNAME']}
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp

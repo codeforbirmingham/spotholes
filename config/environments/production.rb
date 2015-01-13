f6dba77e21d997661e9332a4a6f7fc728798ab9b
Justin Harrison <justin.harrison1337@gmail.com>
2015-01-13 01:29:02 -0600

    tweaking Amazon SES config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,6 +49,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = {host: ENV['SES_HOSTNAME']}
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
 
--- db/migrate/20150101213737_devise_create_admin_users.rb
+++ db/migrate/20150101213737_devise_create_admin_users.rb
@@ -2,7 +2,7 @@ class DeviseCreateAdminUsers < ActiveRecord::Migration
   def migrate(direction)
     super
     # Create a default user
-    AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if direction == :up
+    AdminUser.create!(email: 'justin.harrison1337@gmail.com', password: 'password', password_confirmation: 'password') if direction == :up
   end
 
   def change

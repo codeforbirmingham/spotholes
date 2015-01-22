150f3b4ab8e4c9b3657a24c9af0df523eb8c8b4b
Justin Harrison <justin.harrison1337@gmail.com>
2015-01-22 16:29:22 -0600

    removing hostname config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
-  def default_url_options
-    if Rails.env.production?
-      {:host => 'staging.spothol.es'}
-    else
-      {:host => 'localhost:3000'}
-    end
-  end
-
 end
